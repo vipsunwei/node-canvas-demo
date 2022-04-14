8f8b731cda143c79fed0907e34b7f1934d1da319
example <example@gmail.com>
2022-04-14 17:50:16 +0800

    统一接口上下文路径

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const compression = require("compression");
 const { success } = require("./utils");
 const app = express();
 const port = 3000;
+const contextPath = "/api/node/dataset";
 
 app.use(cors());
 // parse application/x-www-form-urlencoded
@@ -16,27 +17,27 @@ app.use(compression());
 
 // 探空仪廓线图
 const image = require("./image.js");
-app.post("/image", image);
+app.post(contextPath + "/image", image);
 
 // 熔断器高程（度）图
 const heightImage = require("./height_image.js");
-app.post("/heightImage", heightImage);
+app.post(contextPath + "/heightImage", heightImage);
 
 // 设备信息图
 const deviceInfoImage = require("./device_info_image.js");
-app.post("/deviceInfoImage", deviceInfoImage);
+app.post(contextPath + "/deviceInfoImage", deviceInfoImage);
 
 // 历史轨迹数据
 const getHistoryLine = require("./get_history_line.js");
-app.post("/api/node/dataset/gethistoryline", getHistoryLine);
+app.post(contextPath + "/gethistoryline", getHistoryLine);
 
 // 探空仪数据画图，包含属性：温、湿、压、海拔、时间
 const sondeDataForEcharts = require("./sonde_data_for_echarts.js");
-app.post("/api/node/dataset/sondedataforecharts", sondeDataForEcharts);
+app.post(contextPath + "/sondedataforecharts", sondeDataForEcharts);
 
 // 从MongoDB库导出探空仪或熔断器原始数据
 const exportSondeData = require("./export_sonde_data.js");
-app.get("/api/node/dataset/exportsondedata", exportSondeData);
+app.get(contextPath + "/exportsondedata", exportSondeData);
 
 app.listen(port, () => {
   success(`app listening at port:${port}`);
